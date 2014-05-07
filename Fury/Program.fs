e9bb3c5ec1c5e7254993e0e099d5fe0ffc4ed7a2
awostenberg <awostenberg@gmail.com>
2014-05-07 16:41:40 -0600

    Initial

---
+++ Fury/Program.fs
@@ -0,0 +1,75 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+(*
+  general design
+
+  build and run instructions
+    1. atp-get install mono
+    2. git clone repo
+    3. mono build ?
+    4. sh> mono Fury/bin/Debug/program.exe -server
+    5. sh1> monmo Fury/bin/Debug.program.exe -client
+*)
+
+module notes =
+  (*
+    "Fury on.. waiting for the furies to checkin.."
+    "Alecto punishing /tmp for 30 minutes at 10 mb per chunk and rolling over every 100 mb"
+    "Magaera is punishing ..."
+    "Tisiphone is punishing ..."
+    "xyzzy will /not/ punish /tmp for 1 minute at 100 mb per chunk   (yoda rule: config does not two rollovers allow)"
+    "Tisiphone is finished"
+    "Magaera is finished"
+    "Alecto is finished"
+    "xyzzy missed several heartbeats so presumed dead"
+    "general statistics:   Fury   Total chunks     "
+    "Writing stats to csv database..."
+    "Fury off"
+    *)
+    let a=42
+
+
+// start the exectuable in "server" or "client" mode, depending on command line
+
+
+open Server
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    printfn "%A: Fury on: %A"  (System.DateTime.Now) argv
+    printfn "usage: fury  -client  [-to localhost:8090]  -name alecto -duration 30.minutes -chunk 10.mb  -filesys tmp/ -rollover 100.mb"
+    printfn "true usage:  fury <clientId> <chunkMb> <durationSeconds>"
+    let port = 8091
+    let forSeconds = 600.0
+    let ep = System.Net.IPEndPoint(System.Net.IPAddress.Parse "127.0.0.1",port)
+    if argv.Length > 0 then
+      let mb i = (float i) * 1.0<mb>
+          // command line parsing, no error checks
+      let clientId = System.Int32.Parse argv.[0]
+      let chunkSize = System.Int32.Parse argv.[1] |> mb
+      let durationSeconds = System.Int32.Parse argv.[2]
+      printfn "client %d to port %d chunk %A<mb> duration %d<seconds>" clientId port chunkSize durationSeconds
+      let client = Actor.TcpActorClient<Server.Message>(ep)
+      let postSlowly msg =
+          client.Post msg
+          printf "."
+          System.Threading.Thread.Sleep (System.TimeSpan.FromSeconds 1.0)
+      let testMessages = [Start clientId;ServerReport;Rollover (clientId,chunkSize);Stop clientId]
+      postSlowly (Start clientId)
+
+      // inner loop -- rethink as loop not sequence
+
+      let endTimes = System.DateTime.Now + (System.TimeSpan.FromSeconds (float durationSeconds))
+      let timeRemaining msg = System.DateTime.Now < endTimes
+      [1..durationSeconds] |> List.map (fun i -> Rollover (clientId,chunkSize)) |> List.iter postSlowly
+      postSlowly (Stop clientId)
+    else
+      printfn "server at %d for %f seconds" port forSeconds
+      let server = new Actor.TcpActor<Server.Message>(Server.server,ep)
+      let duration = System.TimeSpan.FromSeconds forSeconds
+      printfn "it runs!...for %A" duration
+      System.Threading.Thread.Sleep duration
+      printfn "stop server"   // no, should wait on the exit
+      server.Stop()
+    0 // return an integer exit code
---
+++ Fury/Server.fs
@@ -0,0 +1,83 @@
+﻿module Server
+
+  // for fun I'm trying out message passing style of concurrency, similar to Erlang. No mutable state, so no locks. Just application logic.
+  // messages and agents http://fsharpforfunandprofit.com/posts/concurrency-actor-model/
+  // In Python I'd use the equivalent (research that) or else good old threads and locks for concurrency on mutable client list
+
+  open Microsoft.FSharp.Control
+  open System
+
+
+  [<Measure>] type mb
+  [<Measure>] type seconds
+
+  type ClientState = Green of int<seconds> | Red | Done
+
+  type ClientId = int
+  type Client = {id:ClientId;state:ClientState}
+  type ServerInfo = {totalMb:float<mb>;clientsServed:int}
+
+  type Message = | Start of ClientId | Stop of ClientId | Rollover of ClientId*float<mb> | Heartbeat of ClientId | ServerExit | ServerTick | ServerReport
+
+  //todo:encapsulate in an object
+  let serverInitialState = {totalMb=0.0<mb>;clientsServed=0}
+  let server = MailboxProcessor.Start (fun inbox ->
+      let rec loop(clients,(serverInfo:ServerInfo)) =
+        let rm clientId list = list |> List.filter (fun c -> c.id <> clientId)
+        let add client list = client::list      // second thoughts: dictionary
+        let replace client list = rm client.id list |> add client
+        let isWorking client = match client.state with | Green n -> true | _ -> false
+        let working list = list |> List.filter isWorking |> List.length
+        async { let! envelope = inbox.Receive()
+                let body = envelope
+                match body with 
+                  | Start client ->
+                      printfn "Start client %d" client
+                      let newClients = add {id=client;state=Green 10<seconds>} clients
+                      let newServerInfo = {serverInfo with clientsServed=serverInfo.clientsServed+1}
+                      return! loop(newClients,newServerInfo)
+                  | Stop client ->
+                      printfn "Stop client %d" client
+                      return! loop(rm client clients,serverInfo)
+                  | Rollover (client,m) -> 
+                    printfn "Rollover %d %A<mb>, waiting..." client serverInfo
+                    return! loop(clients,{serverInfo with totalMb=serverInfo.totalMb+m})
+                  | Heartbeat client ->
+                    printfn "Heartbeat %d" client
+                    return! loop(clients,serverInfo)
+                  | ServerReport ->
+                    clients |> List.iter (fun c -> printfn "%A" c)
+                    printfn "total clients %A" serverInfo
+                    return! loop(clients,serverInfo)
+                  | ServerTick ->
+                    // when all clients finished report general stats and finish
+                    if serverInfo.clientsServed > 0 && working clients = 0 then 
+                      inbox.Post(ServerReport)
+                      inbox.Post(ServerExit)
+                    return! loop(clients,serverInfo)
+                  | ServerExit -> 
+                    printfn "server exit"
+                    return() 
+                    }
+      loop ([],{totalMb=0.0<mb>;clientsServed=0}) )
+
+  // finite state machine in the server modeling state of client based on client heartbeats
+  // in Python I'd use the State pattern (http://en.wikipedia.org/wiki/State_pattern) unless there is an idiomatic way
+  module ClientFsm =
+    type ClientHealth = Green of int<seconds> | Red | Done
+
+    // client heartbeat event... always returns to green state
+    let heartbeat s =
+        Green 10<seconds>
+
+    // client tick tock event
+    let tickSecond s =
+        match s with
+          | Green countdown when countdown > 0<seconds> -> Green (countdown - 1<seconds>) // can dwell for this long before presumed dead
+          | Green countdown -> Red
+          | Red -> Red
+          | Done -> Done
+
+    module exploreHeartbeat =
+      Green 10<seconds> |> heartbeat |> tickSecond |> tickSecond |> heartbeat |> ignore
+      Seq.unfold (fun state -> Some(state,tickSecond state) ) (Green 5<seconds>) |> Seq.take 10 |> Seq.toList |> ignore
